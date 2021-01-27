fe89c6a48d12fb8270cc0480a10e1c6e36992c43
Nuria Perez Casas <55450564+nperezcasas@users.noreply.github.com>
2021-01-27 11:28:50 -0600

    Add files via upload

---
+++ Models/BombSquad.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BOMBSQUAD.Models
+{
+    public class BombSquad
+    {
+        internal const int HINT_RADIUS = 3;
+        internal const int BOMB_DENSITY = 3;
+        internal const int HORIZONTAL_CELLS = 16;
+
+        public const int VERTICAL_CELLS = 8;
+        public const string BACK_COLOR = "skyblue";
+        public const string ALT_BACK_COLOR = "steelblue";
+        public const string COLOR = "black";
+        public const string ALT_COLOR = "white";
+
+        public int ArmedCells { get; private set; }
+        public bool GameOver { get; internal set; }
+        public Dictionary<string, Cell> Cells { get; private set; } = new Dictionary<string, Cell> { };
+
+        internal Cell ClickedCell { get; set; }
+        private int UnflaggedBombCount
+        {
+            get
+            {
+                int return_value = 0;
+                foreach (Cell cell in Cells.Values)
+                    if (cell.Armed && !cell.Flagged)
+                        return_value++;
+                return return_value;
+            }
+        }
+
+        public int FlagCount
+        {
+            get
+            {
+                int return_value = 0;
+                foreach (Cell cell in Cells.Values)
+                    if (cell.Flagged)
+                        return_value++;
+                return return_value;
+            }
+        }
+        public double Score
+        {
+            get
+            {
+                return FlagCount / ArmedCells * 100; // For some reason the score doesn't report.  
+            }
+        }
+
+        private void RefreshCells()
+        {
+            ArmedCells = 0;
+            Cells = new Dictionary<string, Cell> { };
+            for (int row = 0; row <= HORIZONTAL_CELLS; ++row)
+            {
+                for (int col = 0; col <= VERTICAL_CELLS;  ++col)
+                {
+                    Cell cell = new Cell(row, col, this);  
+                    Cells.Add(cell.Address, cell);
+                    if (cell.Armed)
+                    {
+                        ArmedCells++;
+                    }
+                }
+            }
+        }
+
+        private void SetInitialHint()
+        {
+            Random rand = new Random();
+            int rand_row = rand.Next(1, BombSquad.VERTICAL_CELLS);
+            int rand_column = rand.Next(1, BombSquad.HORIZONTAL_CELLS);
+            Cell initial_cell = Cells["R" + rand_row + "C" + rand_column];
+            if (!initial_cell.Armed)
+                initial_cell.Clicked = true;
+            else
+                foreach(Cell cell in initial_cell.AdjacentCells)
+                {
+                    if(!cell.Armed)
+                    {
+                        cell.Clicked = true;
+                        break;
+                    }
+                }
+
+        }
+
+        public void Reset()
+        {
+            GameOver = false;
+
+            RefreshCells();
+
+            SetInitialHint();
+        }
+
+        public void ClickCell(string address)
+        {
+            bool keyExists = Cells.ContainsKey(address);
+            if (keyExists)
+            {
+                ClickedCell = Cells[address];
+                ClickedCell.Click();
+            }
+        }
+
+        public void FlagCell(string address)
+        {
+            bool keyExists = Cells.ContainsKey(address);
+            if (keyExists)
+            {
+                ClickedCell = Cells[address];
+                ClickedCell.ToggleFlag();
+            }
+
+            if(UnflaggedBombCount == 0)
+            {
+                GameOver = true;
+            }
+        }
+
+    }
+}
---
+++ Models/Cell.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BOMBSQUAD.Models
+{
+    public class Cell
+    {
+        private readonly BombSquad game;
+        internal readonly bool Armed;
+        public readonly int XCoordinate;
+        public readonly int YCoordinate;
+        private bool Exploded;
+        internal bool Flagged { get; private set; }
+        public bool Clicked;
+
+
+        public string Address
+        {
+            get
+            {
+                return "R" + YCoordinate + "C" + XCoordinate;
+            }
+        }
+
+        public string Content
+        {
+            get
+            {
+                if (Exploded == true)
+                {
+                    return "&#128165;";
+                }
+                if (Flagged == true)
+                {
+                    return "&#128681;";
+                }
+                if (Armed == true)
+                {
+                    return "";
+                }
+                if (Clicked == true)
+                {
+                    return AdjacentBombCount.ToString();
+                }
+                else
+                {
+                    return "";
+                }
+            }
+        }
+        internal List<Cell> AdjacentCells
+        {
+            get
+            {
+                List<Cell> adjCells = new List<Cell>();
+                
+                for (int row = YCoordinate - 1; YCoordinate + 1 >= row; row++)
+                {
+                    for (int col = XCoordinate - 1; XCoordinate + 1 >= col; col++)
+                    {
+                        string address = "R" + row + "C" + col;
+                        if (game.Cells.ContainsKey(address) && address != Address)
+                        {
+                            Cell adj = game.Cells[address];
+                            adjCells.Add(adj);
+                        }
+          
+                    }
+                }
+                return adjCells;
+
+            }
+        }
+
+        private int AdjacentBombCount
+        {
+            get
+            {
+                int return_value = 0;
+                foreach (var cell in AdjacentCells)
+                { 
+                    if (cell.Armed == true)
+                    { 
+                        return_value++;
+
+                    }
+                }
+                return return_value;
+            }
+        }
+
+        internal Cell(int row, int col, BombSquad bombsquad)
+        {
+            game = bombsquad;
+            XCoordinate = row;
+            YCoordinate = col;
+            Random rand = new Random();
+            int num = rand.Next(0, game.Cells.Count);
+            int check = num % BombSquad.BOMB_DENSITY;
+            if (check == 0)
+            {
+                Armed = true;
+            }
+            else
+            {
+                Armed = false;
+            }
+        }
+
+        internal void Click()
+        {
+            if (Flagged == false || this == game.ClickedCell)
+            {
+                Clicked = true;
+                Flagged = false;
+                if (Armed == true && Exploded == false)
+                {
+                    Explode();
+                }
+                foreach (var cell in AdjacentCells)
+                {
+                    
+                    if (!cell.Armed && !cell.Clicked &&
+                        cell.XCoordinate < game.ClickedCell.XCoordinate + BombSquad.HINT_RADIUS &&
+                        cell.XCoordinate > game.ClickedCell.XCoordinate - BombSquad.HINT_RADIUS
+                        &&
+                        cell.YCoordinate < game.ClickedCell.YCoordinate + BombSquad.HINT_RADIUS &&
+                        cell.YCoordinate > game.ClickedCell.YCoordinate - BombSquad.HINT_RADIUS)
+
+                    {
+                        cell.Click();
+                    }
+                }
+            }
+        }
+
+        internal void ToggleFlag()
+        {
+            if(Exploded == false && Flagged == false && Clicked == false && game.FlagCount < game.ArmedCells)
+            {
+                Flagged = true;
+            }
+            else
+            {
+                Flagged = false;
+            }
+        }
+
+        private void Explode()
+        {
+            game.GameOver = true;
+            if (Flagged == !true)
+            {
+                Exploded = true;
+            }
+            foreach (var cell in game.Cells.Values)
+            {
+                if (cell.Armed && !cell.Exploded && !cell.Flagged) 
+                {
+                    cell.Explode(); 
+                }
+            }
+        }
+
+    }
+}
