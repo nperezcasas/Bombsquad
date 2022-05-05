81b49497ccf44db6203e366520fcf600981b0c53
nperezcasas <55450564+nperezcasas@users.noreply.github.com>
2022-05-04 19:50:35 -0500

    Initial check-in of module BOMBSQUAD

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using BOMBSQUAD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BOMBSQUAD.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly BombSquad game = new BombSquad();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Reset()
+        {
+            game.Reset();
+            return View("Game", game);
+        }
+
+        public IActionResult Click(string cell)
+        {
+            game.ClickCell(cell);
+            return View("Game", game);
+        }
+
+        public IActionResult Flag(string cell)
+        {
+            game.FlagCell(cell);
+            return View("Game", game);
+        }
+    }
+}
